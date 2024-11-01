e8665477f92cb78f6d25c8d78e855666cab15c8d
Eric <eric.espie@eboreal.com>
2024-11-01 09:42:43 +0100

    Not aiming outside the background

--- scenes/gameplay/states/aim.gd
+++ scenes/gameplay/states/aim.gd
@@ -3,6 +3,13 @@ extends "res://StateMachine/state.gd"
 @export
 var run_state: State
 
+var game_area : Sprite2D
+
+func enter() -> void:
+	# Store the game area to restrict the aiming
+	for bg in get_tree().get_nodes_in_group("GameArea"):
+		game_area = bg
+
 func process_input(event: InputEvent) -> State:	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
 		#var pos = make_input_local(event).position
@@ -10,6 +17,7 @@ func process_input(event: InputEvent) -> State:	# Mouse in viewport coordinates.
 			EventBus.sigStopAiming.emit()
 			return run_state
 	elif event is InputEventMouseMotion:
-		EventBus.sigMouseAiming.emit(event.position)
+		if game_area.get_rect().has_point(game_area.to_local(event.position)):
+			EventBus.sigMouseAiming.emit(event.position)
 		
 	return null
--- scenes/gameplay/states/idle.gd
+++ scenes/gameplay/states/idle.gd
@@ -3,9 +3,16 @@ extends "res://StateMachine/state.gd"
 @export
 var aim_state : State
 
+var game_area : Sprite2D
+
+func enter() -> void:
+	# Store the game area to restrict the aiming
+	for bg in get_tree().get_nodes_in_group("GameArea"):
+		game_area = bg
+
 func process_input(event: InputEvent) -> State:
 	if event is InputEventMouseButton:
-		if event.is_pressed():
+		if event.is_pressed() and game_area.get_rect().has_point(game_area.to_local(event.position)):
 			return aim_state
 		
 	# Not relevant
