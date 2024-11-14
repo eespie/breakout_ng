a6c62bc3f9607d380b48272ac172b00160376a0f
Eric <eric.espie@eboreal.com>
2024-11-14 19:21:48 +0100

    earn coins when screen is wiped out

--- scenes/gameplay/replay_bonus_message.gd
+++ scenes/gameplay/replay_bonus_message.gd
@@ -1,17 +1,22 @@
 extends Label
 
 var tween : Tween
+@onready var coins_earned_label = $CoinsEarned
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	EventBus.sigReplayBonusUpdated.connect(_on_replay_bonus_updated)
+	EventBus.sigAddScorePoints.connect(_on_points_added)
+	set_modulate(Color.TRANSPARENT)
 
 func _on_replay_bonus_updated(bonus : int):
 	if bonus > 0:
 		if tween:
 			tween.kill()
-		show()
 		set_modulate(Color.WHITE)
 		tween = get_tree().create_tween()
 		tween.tween_property(self, "modulate", Color.WHITE, 1.5)
 		tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
+
+func _on_points_added(points: int):
+	coins_earned_label.set_text("%d coins earned" % points)
