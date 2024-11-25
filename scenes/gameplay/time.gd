ce0f3762204394c3e5c587c44abb029daae166cc
Eric <eric.espie@eboreal.com>
2024-11-25 21:15:57 +0100

    fix time and remove vsync

--- scenes/gameplay/time.gd
+++ scenes/gameplay/time.gd
@@ -6,7 +6,7 @@ var last_elapsed = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var time = Time.get_datetime_dict_from_system()
-	set_text("%2d:%2d" % [time['hour'], time['minute']])
+	set_text("%02d:%02d" % [time['hour'], time['minute']])
 	elapsed = time['second']
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
